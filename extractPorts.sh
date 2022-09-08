479dc93bfd0669d17495649d21d1de05e069f138
¨Jaider <lovemyhatersv02@gmail.com>
2022-09-07 21:35:41 -0400

    Subida Inicial

---
+++ extractPorts.sh
@@ -0,0 +1,18 @@
+# Modo de Uso:
+
+# Realizar el escaneo como hacemos normalmente con nmap salvo la excepción de que
+# debemos exportar los resultados en formato grepeable "-oG" especificicando el nombre
+
+# Ejecutamos la función pasandole como parámetro el fichero de salida del escaneo
+# extractPorts "nombreDelFichero"
+
+function extractPorts(){
+	ports="$(cat $1 | grep -oP '\d{1,5}/open' | awk '{print $1}' FS='/' | xargs | tr ' ' ',')"
+	ip_address="$(cat $1 | grep -oP '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}' | sort -u | head -n 1)"
+	echo -e "\n[*] Extracting information...\n" > extractPorts.tmp
+	echo -e "\t[*] IP Address: $ip_address"  >> extractPorts.tmp
+	echo -e "\t[*] Open ports: $ports\n"  >> extractPorts.tmp
+	echo $ports | tr -d '\n' | xclip -sel clip
+	echo -e "[*] Ports copied to clipboard\n"  >> extractPorts.tmp
+	cat extractPorts.tmp; rm extractPorts.tmp
+}
